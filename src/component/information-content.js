6e2348e2b1306985724da0cb9c2845d48dcb051b
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-02 08:21:22 +0200

    Add information section with two component for content and title

---
+++ src/component/information-content.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function InformationContent(){
+return(
+            
+                <div className="informations-item">
+                    <div className="icon">
+                        <img src="https://img.icons8.com/color/48/000000/javascript.png" alt="Skill logo"/>
+                    </div>
+                    <p>JavaScript</p>
+                    <p>Some text about js</p>
+                </div>
+          
+)
+}
+export default InformationContent;
---
+++ src/component/information-title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InformationTitle(){
+return(
+            
+    <div className="informations-title">
+    <p className="section-title">Skills</p>
+</div>
+)
+}
+export default InformationTitle;
