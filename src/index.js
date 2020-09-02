20323e139ae0e2f5643a22617a158ddd5965fc78
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-02 08:18:17 +0200

    Add the basic js file index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Start from "./component/start"
+import Skills from "./component/skills"
+
+ReactDOM.render(
+    <div><Start/>
+    <Skills />
+
+    </div>
+
+    ,document.getElementById("root"));
