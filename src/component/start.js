671fa80a9efbf2e9d23a359454c0130319465ce1
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-02 08:19:48 +0200

    Add start page section to the portfolio

---
+++ src/component/start.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Start (){
+    return (
+    <section id="start">
+        <div className="start container">
+            <div>
+                <p>Hello, <span></span></p>
+                <p>My Name Is <span></span></p>
+                <h1>Rufaida <span></span></h1>
+                <a href="#" type="button" className="cta">Portfolio</a>
+            </div>   
+        </div>
+  </section>
+    )
+}
+export default Start;
