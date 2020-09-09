f4e849560aba08990bdbff669a57a3cb20021d56
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-09 09:39:23 +0200

    Add informationTitle component to use it as title with information components

---
+++ src/component/InformationTitle.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function InformationTitle(props){
+return(       
+    <div className="informations-title">
+    <p className="section-title">{props.title}</p>
+</div>
+)
+}
+export default InformationTitle;
