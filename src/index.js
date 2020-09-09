add54243244bf819bbc3347c557e3982fbf9738b
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-09 09:34:00 +0200

    Import Education component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Start from "./component/start"
-import Skills from "./component/skills"
-
+import Start from "./component/Start"
+import Skills from "./component/Skills"
+import Education from "./component/Education"
 ReactDOM.render(
-    <div><Start/>
-    <Skills />
-
+    <div>
+        <Start />
+        <Skills />
+        <Education />
     </div>
-
     ,document.getElementById("root"));
