18fcb2eb3020ec745dbb8d63d296070d0b748bd4
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-16 21:56:13 +0200

    Add Projects section

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom";
 import Start from "./component/Start"
 import Skills from "./component/Skills"
 import Education from "./component/Education"
+import Projects from "./component/Projects"
 ReactDOM.render(
     <div>
         <Start />
         <Skills />
         <Education />
+        <Projects />
     </div>
     ,document.getElementById("root"));
