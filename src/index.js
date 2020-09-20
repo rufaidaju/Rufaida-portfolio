cc984fa3512bdd6a8d2bac316bec8c9ea5ff9ac6
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-20 23:01:52 +0200

    import Header component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Header from "./component/Header"
 import Start from "./component/Start"
 import Skills from "./component/Skills"
 import Education from "./component/Education"
 import Projects from "./component/Projects"
 ReactDOM.render(
     <div>
+        <Header />
         <Start />
         <Skills />
         <Education />
