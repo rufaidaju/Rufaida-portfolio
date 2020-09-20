428febb0e576596caaa488db0d88953b9caf17b4
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-21 00:42:57 +0200

    Add handleClick function to navbar in mobile

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React,{Component} from "react";
 
-function Header (){
-    return(
-        <section id="header">
-        <div class="header container">
-          <div class="nav-bar">
-            <div class="brand">
-              <a href="#hero"><h1><span>R</span>ufaida <span>J</span>umaa</h1></a>
+class Header extends Component{
+    constructor(){
+        super();
+        this.state={}
+    }
+    
+    handleClick(){
+        const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+        const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+        
+        hamburger.addEventListener('click', () => {
+	    hamburger.classList.toggle('active');
+	    mobile_menu.classList.toggle('active');
+        }); 
+        
+        return
+    }
+
+    render(){
+        return(
+          <section id="header">
+            <div className="header container">
+              <div className="nav-bar">
+                <div className="brand">
+                  <a href="#hero"><h1><span>R</span>ufaida <span>J</span>umaa</h1></a>
+                </div>
+                <div className="nav-list">
+                  <button className="hamburger" onClick={this.handleClick}><div className="bar"></div></button>
+                  <ul>
+                    <li><a href="#start" data-after="Home">Home</a></li>
+                    <li><a href="#services" data-after="Skills">Skills</a></li>
+                    <li><a href="#about" data-after="Education">Education</a></li>
+                    <li><a href="#projects" data-after="Projects">Projects</a></li>
+                    <li><a href="#contact" data-after="Contact">Contact</a></li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div class="nav-list">
-              <div class="hamburger"><div class="bar"></div></div>
-              <ul>
-                <li><a href="#start" data-after="Home">Home</a></li>
-                <li><a href="#services" data-after="Skills">Skills</a></li>
-                <li><a href="#about" data-after="Education">Education</a></li>
-                <li><a href="#projects" data-after="Projects">Projects</a></li>
-                <li><a href="#contact" data-after="Contact">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section>
-    )
+          </section>
+        )
+    } 
 }
 export default Header;
