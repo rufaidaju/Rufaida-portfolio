682cfd100e4ab7c2179b17ba63b82631c281fcd5
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-22 15:40:23 +0200

    Add Footer elements

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,6 +4,37 @@ import React from "react";
 
 function Footer(){
 return(
-    
+    <section id="footer">
+  <div class="footer  container">
+    <div class="brand"><p><span>R</span>ufaida <span>J</span>umaa</p></div>
+    <p>Contact me on:</p>
+    <div class="social-icon">
+      <div class="social-item">
+        <a href="20710141">
+            <img src="https://img.icons8.com/bubbles/50/000000/phone.png" alt="Phone icon"/>
+            <span>20710141</span>
+        </a>
+      </div>
+      <div class="social-item">
+        <a href="mailto:eng.rufaidajumaa@gmail.com">
+            <img src="https://img.icons8.com/bubbles/50/000000/sent.png" alt="E-mail icon"/>
+            <span>E-mail</span>
+        </a>
+      </div>
+      <div class="social-item">
+        <a href="https://github.com/rufaidaju" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github icon"/>
+            <span>Github</span>
+        </a>
+      </div>
+      <div class="social-item">
+        <a href="https://www.linkedin.com/in/rufaida-jumaa-a15ba4142" target="_blank"  rel="noopener noreferrer">
+            <img src="https://img.icons8.com/nolan/64/linkedin-circled.png" alt="Linkedin icon"/>
+            <span>Linkedin</span></a>
+      </div>
+    </div>
+    <small>&copy; Copyright 2018, Example Corporation</small> 
+  </div>
+</section>
 )}
 export default Footer;
