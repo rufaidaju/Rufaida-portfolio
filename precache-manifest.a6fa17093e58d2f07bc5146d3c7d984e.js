c612546225ee766c3ea4a43bac3c276516cc4b84
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-22 22:31:27 +0200

    Updates

---
+++ precache-manifest.a6fa17093e58d2f07bc5146d3c7d984e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbfc8f1d5aef116f5fec6bbb9bedf74",
+    "url": "/Rufaida-portfolio/index.html"
+  },
+  {
+    "revision": "fa7b6ca8621cb4434660",
+    "url": "/Rufaida-portfolio/static/js/2.3b67e28b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Rufaida-portfolio/static/js/2.3b67e28b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89d3224c3b7c5b1e12a",
+    "url": "/Rufaida-portfolio/static/js/main.18933e53.chunk.js"
+  },
+  {
+    "revision": "31727ee37ce3fc1f41cb",
+    "url": "/Rufaida-portfolio/static/js/runtime-main.f8b10755.js"
+  },
+  {
+    "revision": "6b99945a29a5449a96b71de77e80f105",
+    "url": "/Rufaida-portfolio/static/media/giphy.6b99945a.PNG"
+  },
+  {
+    "revision": "76b0437a99fd2122088a0fdb257a33eb",
+    "url": "/Rufaida-portfolio/static/media/meal-sharing.76b0437a.PNG"
+  },
+  {
+    "revision": "fcfd0146434a3a91abc6d0120ecef758",
+    "url": "/Rufaida-portfolio/static/media/move-circles.fcfd0146.PNG"
+  },
+  {
+    "revision": "32a89b0aed76000873a6718c851701ad",
+    "url": "/Rufaida-portfolio/static/media/random-circles.32a89b0a.PNG"
+  },
+  {
+    "revision": "1deccad4884ca835ad313401bcf4d00b",
+    "url": "/Rufaida-portfolio/static/media/weather.1deccad4.PNG"
+  }
+]);
