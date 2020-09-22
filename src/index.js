18b7afcf450cb5e0145c540ec8e0634fca529ecd
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-22 15:17:20 +0200

    Import Footer component

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Start from "./component/Start"
 import Skills from "./component/Skills"
 import Education from "./component/Education"
 import Projects from "./component/Projects"
+import Footer from "./component/Footer"
+
 ReactDOM.render(
     <div>
         <Header />
@@ -13,5 +15,6 @@ ReactDOM.render(
         <Skills />
         <Education />
         <Projects />
+        <Footer />
     </div>
     ,document.getElementById("root"));
