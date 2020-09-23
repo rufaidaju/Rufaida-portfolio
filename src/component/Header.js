68065c325780494f04ae0c5225198206cd03c8e5
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-23 23:40:48 +0200

    Fix mobile menu toggle

--- src/component/Header.js
+++ src/component/Header.js
@@ -10,12 +10,9 @@ class Header extends Component{
         const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
         const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
         
-        hamburger.addEventListener('click', () => {
-	    hamburger.classList.toggle('active');
-	    mobile_menu.classList.toggle('active');
-        }); 
-        
-        return
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active'); 
+      return
     }
 
     render(){
@@ -24,16 +21,16 @@ class Header extends Component{
             <div className="header container">
               <div className="nav-bar">
                 <div className="brand">
-                  <a href="#hero"><h1><span>R</span>ufaida <span>J</span>umaa</h1></a>
+                  <a href="#start"><h1><span>R</span>ufaida <span>J</span>umaa</h1></a>
                 </div>
                 <div className="nav-list">
                   <button className="hamburger" onClick={this.handleClick}><div className="bar"></div></button>
                   <ul>
-                    <li><a href="#start" data-after="Home">Home</a></li>
-                    <li><a href="#skills" data-after="Skills">Skills</a></li>
-                    <li><a href="#education" data-after="Education">Education</a></li>
-                    <li><a href="#projects" data-after="Projects">Projects</a></li>
-                    <li><a href="#footer" data-after="Contact">Contact</a></li>
+                    <li><a href="#start" data-after="Home" onClick={this.handleClick}>Home</a></li>
+                    <li><a href="#skills" data-after="Skills" onClick={this.handleClick}>Skills</a></li>
+                    <li><a href="#education" data-after="Education" onClick={this.handleClick}>Education</a></li>
+                    <li><a href="#projects" data-after="Projects" onClick={this.handleClick}>Projects</a></li>
+                    <li><a href="#footer" data-after="Contact" onClick={this.handleClick}>Contact</a></li>
                   </ul>
                 </div>
               </div>
