f1b89b9b97b48d8dd93611335b291242aa6e8a95
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-23 15:08:38 +0200

    Edit copyright,add sms contact,edit phone contact

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,35 +5,41 @@ import React from "react";
 function Footer(){
 return(
     <section id="footer">
-  <div class="footer  container">
-    <div class="brand"><p><span>R</span>ufaida <span>J</span>umaa</p></div>
+  <div className="footer  container">
+    <div className="brand"><p><span>R</span>ufaida <span>J</span>umaa</p></div>
     <p>Contact me on:</p>
-    <div class="social-icon">
-      <div class="social-item">
-        <a href="20710141">
+    <div className="social-icon">
+      <div className="social-item">
+        <a href="tel:+4520710141">
             <img src="https://img.icons8.com/bubbles/50/000000/phone.png" alt="Phone icon"/>
             <span>20710141</span>
         </a>
       </div>
-      <div class="social-item">
+      <div className="social-item">
+        <a href="sms:+4520710141">
+        <img src="https://img.icons8.com/bubbles/50/000000/sent.png" alt="SMS icon"/>
+            <span>Message</span>
+        </a>
+      </div>
+      <div className="social-item">
         <a href="mailto:eng.rufaidajumaa@gmail.com">
-            <img src="https://img.icons8.com/bubbles/50/000000/sent.png" alt="E-mail icon"/>
+            <img src="https://img.icons8.com/bubbles/50/000000/important-mail.png" alt="E-mail icon"/>
             <span>E-mail</span>
         </a>
       </div>
-      <div class="social-item">
+      <div className="social-item">
         <a href="https://github.com/rufaidaju" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github icon"/>
+            <img src="https://img.icons8.com/bubbles/50/000000/github.png" alt="Github icon"/>
             <span>Github</span>
         </a>
       </div>
-      <div class="social-item">
+      <div className="social-item">
         <a href="https://www.linkedin.com/in/rufaida-jumaa-a15ba4142" target="_blank"  rel="noopener noreferrer">
-            <img src="https://img.icons8.com/nolan/64/linkedin-circled.png" alt="Linkedin icon"/>
+            <img src="https://img.icons8.com/bubbles/50/000000/linkedin.png" alt="Linkedin icon"/>
             <span>Linkedin</span></a>
       </div>
     </div>
-    <small>&copy; Copyright 2018, Example Corporation</small> 
+    <small>&copy; Copyright 2020.Rufaida</small> 
   </div>
 </section>
 )}
