27d1bd8fdeec0ffdf8b814fefdd4e625bc9b0831
Rufaida <eng.rufaidajumaa@gmail.com>
2020-10-14 09:51:59 +0200

    Fix CV download failed

--- src/component/start.js
+++ src/component/start.js
@@ -1,15 +1,15 @@
 import React from "react";
-
+import rufaida from "../data/rufaida.pdf"
 
 function Start (){
     return (
     <section id="start">
         <div className="start container">
-            <div>
+            <div> 
                 <p>Hello, <span></span></p>
                 <p>My Name Is <span></span></p>
                 <p>Rufaida <span></span></p>
-                <a href="../data/rufaida" download="RufaidaCV.pdf" className="cta">Download CV</a>
+                <a href={rufaida} download className="cta">Download CV</a>
             </div>   
         </div>
   </section>
